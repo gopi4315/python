5feb4324902a2ad59ed679420c4ccb4fda77a3fe
GOPIKRISHNA <116140243+gopi4315@users.noreply.github.com>
2022-10-25 22:38:22 +0530

    Add files via upload

---
+++ stringmethods.py
@@ -0,0 +1,11 @@
+#capitalization method
+#a = "my method is capitalization"
+#print(a.capitalize())
+
+#casefold method
+#b = "SECOND METHOD IS CASEFOLD"
+#print(b.casefold())
+
+#count()
+#c = "i love marvel comics, comics are best in childhood"
+#print(c.count("comics"))
