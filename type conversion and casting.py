e26f271fd9829a1fe4c9b601b33a3c92e58b7f8f
GOPIKRISHNA <116140243+gopi4315@users.noreply.github.com>
2022-10-26 23:39:36 +0530

    Add files via upload

---
+++ type conversion and casting.py
@@ -0,0 +1,17 @@
+#type conversion and type casting
+#when a string is having a float value directly converting into integer is not possible.
+#we get a value error direct conversion of float to integer.
+#x = '8.5'
+#print(float(x))
+#direct conversion of float to int is not possible so we have to create a variable and convert into int.
+#y = float(x)
+#print(int(y))
+a = 9 ; b = 7#when we need a particular variable we simple pass the variable we want.
+#print(a)
+#print(b)# OR
+#print('%d'%a)#don't keep space infront of the %d.
+#print('%d'%b)
+#print(a,b)# comma gives one space b/w a and b
+#print('%d %d'%(a,b))# for printing both the value.
+#print('%f'%a)  #by print%f we get the values in  the float with six zero's.(9.000000)
+#print('%.1f'%a) #by adding %.1f we get only one zero i.e(9.0) similarily %.2f %.3f etc. 
